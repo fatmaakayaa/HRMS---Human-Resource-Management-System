85580340d9b232124d1b083306aab591d36341c0
Fatma <frukiyekayaa@gmail.com>
2021-05-12 22:32:02 +0300

    hrms

---
+++ main/java/kodlamaio/hrms/api/controllers/UsersController.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.UserService;
+import kodlamaio.hrms.entities.concretes.User;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UsersController {
+	private UserService userService;
+	
+	public UsersController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@GetMapping("/getall")
+	public List<User> getAll(){
+		return userService.getAll();
+	}
+	
+}
